3774d6814d7ec46e1e89905a33e505fc0c61173c
Ivan <86902525+JoStooop@users.noreply.github.com>
2021-09-15 13:52:34 +0300

    part - 1

---
+++ js/initialize-swiper.js
@@ -0,0 +1,20 @@
+let swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1.3,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ js/read-more.js
@@ -0,0 +1,13 @@
+function openbox(id) {
+
+display = document.getElementById(id).style.display;
+
+    if (display=='none') {
+        document.getElementById(id).style.display='flex';
+
+    } else{
+
+        document.getElementById(id).style.display='none';
+        
+    }
+}
